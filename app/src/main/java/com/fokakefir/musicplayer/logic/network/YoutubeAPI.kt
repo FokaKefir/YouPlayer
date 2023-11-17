32fe18f11c710ea406e3be795aba7ed5d7c5d17d
Babos Dávid <40315291+FokaKefir@users.noreply.github.com>
2023-11-17 22:26:02 +0100

    network subdirectory java files to kotlin

--- app/src/main/java/com/fokakefir/musicplayer/logic/network/YoutubeAPI.kt
+++ app/src/main/java/com/fokakefir/musicplayer/logic/network/YoutubeAPI.kt
@@ -1,48 +1,42 @@
-package com.fokakefir.musicplayer.logic.network;
+package com.fokakefir.musicplayer.logic.network
 
-import com.fokakefir.musicplayer.model.youtube.YoutubeVideos;
+import com.fokakefir.musicplayer.model.youtube.YoutubeVideos
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Url
 
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Url;
-
-public class YoutubeAPI {
-
-    public static final int YOUTUBE_ITAG_VIDEO_480P = 18;
-    public static final int YOUTUBE_ITAG_AUDIO_50K = 249;
-    public static final int YOUTUBE_ITAG_AUDIO_160K = 251;
-    public static final int YOUTUBE_ITAG_AUDIO_128K = 140;
-
-    public static final String BASE_URL = "https://www.googleapis.com/youtube/v3/";
-    public static final String SEARCH = "search";
-    public static final String KEY = "?key=AIzaSyBja95PvyW-AFi3T2a8fuua8wDXwUEdcu0";
-    public static final String CHANNEL_ID = "&channelId=UCR18dAVRt3-rYqFd3ac8zAg";
-    public static final String MAX_RESULTS = "&maxResults=16";
-    public static final String ORDER = "&order=relevance";
-    public static final String PART = "&part=snippet";
-
-    public static final String QUERY = "&q=";
-    public static final String TYPE = "&type=video";
-
-    public interface YoutubeVideosRequest {
-        @GET
-        Call<YoutubeVideos> getYT(@Url String url);
-    }
-
-    private static YoutubeVideosRequest request = null;
-
-    public static YoutubeVideosRequest getRequest() {
-        if (request == null) {
-            Retrofit retrofit = new Retrofit.Builder()
+object YoutubeAPI {
+    const val YOUTUBE_ITAG_VIDEO_480P = 18
+    const val YOUTUBE_ITAG_AUDIO_50K = 249
+    const val YOUTUBE_ITAG_AUDIO_160K = 251
+    const val YOUTUBE_ITAG_AUDIO_128K = 140
+    const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+    const val SEARCH = "search"
+    const val KEY = "?key=AIzaSyBja95PvyW-AFi3T2a8fuua8wDXwUEdcu0"
+    const val CHANNEL_ID = "&channelId=UCR18dAVRt3-rYqFd3ac8zAg"
+    const val MAX_RESULTS = "&maxResults=16"
+    const val ORDER = "&order=relevance"
+    const val PART = "&part=snippet"
+    const val QUERY = "&q="
+    const val TYPE = "&type=video"
+    @JvmStatic
+    var request: YoutubeVideosRequest? = null
+        get() {
+            if (field == null) {
+                val retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-
-            request = retrofit.create(YoutubeVideosRequest.class);
+                    .build()
+                field = retrofit.create(YoutubeVideosRequest::class.java)
+            }
+            return field
         }
-        return request;
-    }
+        private set
 
+    interface YoutubeVideosRequest {
+        @GET
+        fun getYT(@Url url: String?): Call<YoutubeVideos?>?
+    }
 }
